950ce4b861bc57722bb3b61732135a9d25c7bb5f
Anastasiia Volichenko <anastasiia.volichenko@gmail.com>
2026-09-05 17:25:08 +0100

    a new commit with first implementation (Book) + create a firstTestBook

--- online-book-shop/src/main/java/work/onlinebookshop/OnlineBookShopApplication.java
+++ online-book-shop/src/main/java/work/onlinebookshop/OnlineBookShopApplication.java
@@ -1,12 +1,20 @@
 package work.onlinebookshop;
 
+import java.math.BigDecimal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import work.onlinebookshop.model.Book;
+import work.onlinebookshop.service.BookService;
 
 @SpringBootApplication
 public class OnlineBookShopApplication {
+    private final BookService bookService;
+
+    public OnlineBookShopApplication(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookShopApplication.class, args);
@@ -17,7 +25,17 @@ public class OnlineBookShopApplication {
         return new CommandLineRunner() {
             @Override
             public void run(String... args) throws Exception {
+                Book firstTestBook = new Book();
+                firstTestBook.setTitle("firstTestBook");
+                firstTestBook.setAuthor("firstTestAuthor");
+                firstTestBook.setIsbn("firstTestIsbn");
+                firstTestBook.setPrice(BigDecimal.valueOf(10));
+                firstTestBook.setDescription("firstTestDescription");
+                firstTestBook.setCoverImage("firstTestCoverImage");
 
+                bookService.save(firstTestBook);
+                bookService.findAll().forEach(book ->
+                        System.out.println(book.getTitle() + " - " + book.getAuthor()));
             }
         };
     }
