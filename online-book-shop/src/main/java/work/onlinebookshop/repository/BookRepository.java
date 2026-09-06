1e7e225189a2d0111f2e2562996e546c9a4a35f3
Anastasiia Volichenko <anastasiia.volichenko@gmail.com>
2026-09-06 18:42:38 +0100

    the first commit from hm-02 with new implementations

--- online-book-shop/src/main/java/work/onlinebookshop/repository/BookRepository.java
+++ online-book-shop/src/main/java/work/onlinebookshop/repository/BookRepository.java
@@ -1,10 +1,13 @@
 package work.onlinebookshop.repository;
 
 import java.util.List;
+import java.util.Optional;
 import work.onlinebookshop.model.Book;
 
 public interface BookRepository {
     Book save(Book book);
 
     List<Book> findAll();
+
+    Optional<Book> findById(Long id);
 }
