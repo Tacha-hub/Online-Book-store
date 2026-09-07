327bab85fc9e50173d2df54f7cae708c56cdf246
Anastasiia Volichenko <anastasiia.volichenko@gmail.com>
2026-09-07 16:59:35 +0100

    open pr-02 from branch hw-03

--- online-book-shop/src/main/java/work/onlinebookshop/controller/BookController.java
+++ online-book-shop/src/main/java/work/onlinebookshop/controller/BookController.java
@@ -2,11 +2,15 @@ package work.onlinebookshop.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import work.onlinebookshop.dto.BookDto;
 import work.onlinebookshop.dto.CreateBookRequestDto;
@@ -31,5 +35,17 @@ public class BookController {
     @PostMapping
     public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBook(@PathVariable Long id,
+            @RequestBody CreateBookRequestDto bookDto) {
+        return bookService.update(id, bookDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteById(id);
     }
 }
--- online-book-shop/src/main/java/work/onlinebookshop/repository/BookRepository.java
+++ online-book-shop/src/main/java/work/onlinebookshop/repository/BookRepository.java
@@ -1,13 +1,8 @@
 package work.onlinebookshop.repository;
 
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import work.onlinebookshop.model.Book;
 
-public interface BookRepository {
-    Book save(Book book);
+public interface BookRepository extends JpaRepository<Book, Long> {
 
-    List<Book> findAll();
-
-    Optional<Book> findById(Long id);
 }
