c9cfb54ddd867f41bdb803ad552fb5833e9ab20c
Anastasiia Volichenko <anastasiia.volichenko@gmail.com>
2026-09-08 22:08:46 +0100

    added validation + CustomGlobalExceptionHandler

--- online-book-shop/src/main/java/work/onlinebookshop/dto/CreateBookRequestDto.java
+++ online-book-shop/src/main/java/work/onlinebookshop/dto/CreateBookRequestDto.java
@@ -1,14 +1,30 @@
 package work.onlinebookshop.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+
+    @NotBlank(message = "Title must not be blank")
     private String title;
+
+    @NotBlank(message = "Author must not be blank")
     private String author;
+
+    @NotBlank(message = "ISBN must not be blank")
     private String isbn;
+
+    @NotNull(message = "Price is necessary")
+    @Positive(message = "Price must be greater than zero")
     private BigDecimal price;
+
+    @NotBlank(message = "Description must not be blank")
     private String description;
+
+    @NotBlank(message = "CoverImage must not be blank")
     private String coverImage;
 }
--- online-book-shop/src/main/java/work/onlinebookshop/model/Book.java
+++ online-book-shop/src/main/java/work/onlinebookshop/model/Book.java
@@ -33,6 +33,6 @@ public class Book {
     private String description;
     private String coverImage;
 
-    @Column(name = "is_deleted", nullable = false)
+    @Column(nullable = false)
     private boolean deleted = false;
 }
